d6e364d8034d167c5525b747c7765677cfba4e3c
Anshika Gupta <63110987+Anshika66-Gupta@users.noreply.github.com>
2020-11-26 23:27:22 +0530

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import LogNorm
+dx, dy =0.015, 0.05
+x = np.arange(-4.0, 4.0, dx)
+y = np.arange(-4.0, 4.0, dy)
+X, Y = np.meshgrid(x,y)
+extent = np.min(x), npmax(x), np.min(y), np.max(y)
+Z1 = np.add.outer(range(8), range(8)) % 2
+plt.imshow(Z1, cmap="binary_r",
+           interpolation='nearest',
+           extent=extent, alpha=1)
+def copyasssignment(x,y):
+    return(1-x / 2 + x ** 5 + y ** 6) * np.exp(-(x ** 2 + y **2))
+Z2 = copyassignment(X,Y)
+plt.imshow(Z2, alpha=0.7, interpolation='bilinear', extent=extent)
+plt.cool()
+plt.title('matpotlib.pyplot.cool() function Example', fontweight="bold")
+plt.show()
